09d4fe03ce3e63e7b2e4aeb3c5a6445b284f480d
Sebastião Teixeira <steixeira2004@gmail.com>
2025-04-06 01:25:35 +0100

    Get rabbitmq url from .env

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const amqp = require('amqplib');
-const webPush = require('web-push');
-const { Client } = require('pg');
-require('dotenv').config();
+const amqp = require("amqplib");
+const webPush = require("web-push");
+const { Client } = require("pg");
+require("dotenv").config();
 
 webPush.setVapidDetails(
-  'mailto:example@yourdomain.com',
+  "mailto:example@yourdomain.com",
   process.env.VAPID_PUBLIC_KEY,
   process.env.VAPID_PRIVATE_KEY
 );
 
-const queue = 'webpush_queue';
-const amqpUrl = `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@localhost`;
+const queue = "webpush_queue";
+const amqpUrl = process.env.RABBITMQ_URL || "amqp://mq:5672";
 
 const client = new Client({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
+  port: process.env.POSTGRES_PORT || 5432,
 });
 
 client.connect().catch((error) => {
-  console.error('Failed to connect to PostgreSQL:', error.message);
+  console.error("Failed to connect to PostgreSQL:", error.message);
   process.exit(1); // Exit the process if the database connection fails
 });
 
 async function fetchUserSubscription(userId) {
   try {
-    const res = await client.query('SELECT subscription FROM webpush_notifications_subscriptions WHERE user_id = $1', [userId]);
+    const res = await client.query(
+      "SELECT subscription FROM webpush_notifications_subscriptions WHERE user_id = $1",
+      [userId]
+    );
     if (res.rows.length > 0) {
       return res.rows[0].subscription;
     } else {
       return null;
     }
   } catch (error) {
-    console.error('Error fetching subscription from database:', error);
+    console.error("Error fetching subscription from database:", error);
     return null;
   }
 }
@@ -42,9 +45,9 @@ async function fetchUserSubscription(userId) {
 async function sendWebPush(subscription, message) {
   try {
     await webPush.sendNotification(subscription, JSON.stringify(message));
-    console.log('Notification sent successfully');
+    console.log("Notification sent successfully");
   } catch (error) {
-    console.error('Error sending notification:', error);
+    console.error("Error sending notification:", error);
   }
 }
 
@@ -62,7 +65,7 @@ async function consumeMessages() {
         console.log(message);
 
         const { recipient, payload } = message;
-        
+
         const subscription = await fetchUserSubscription(recipient);
 
         if (subscription) {
@@ -75,7 +78,7 @@ async function consumeMessages() {
       }
     });
   } catch (error) {
-    console.error('Error in RabbitMQ consumer:', error);
+    console.error("Error in RabbitMQ consumer:", error);
   }
 }
 
